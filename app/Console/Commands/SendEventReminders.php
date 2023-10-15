888f3bb01f1fdd2d9729570b1a3431927fd79a27
bogdanGR <fant1k450@gmail.com>
2023-10-15 11:24:23 +0300

    Use queue

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -35,7 +36,9 @@ class SendEventReminders extends Command
         $this->info("Found {$eventsCount} {$eventLabel}");
 
         $events->each(fn ($event) => $event->attendees->each(
-            fn ($attendee) => $this->info("Notifying the user {$attendee->user_id}")));
+            fn ($attendee) =>$attendee->user->notify(
+                new EventReminderNotification($event)
+            ) ));
         $this->info('Reminder notifications sent successfully');
     }
 }
